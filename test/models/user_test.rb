3f01687353343c72493fba11aa0da56828796c2c
Spencer <scarvill91@gmail.com>
2015-02-19 20:59:43 -0600

    added validation for minimum length of User password

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -55,4 +55,9 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user.email = @user.email.upcase
     assert_not duplicate_user.valid?
   end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
 end
