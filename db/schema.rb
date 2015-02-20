564eee3340b3c9d93ee375ec61f1352976439c6a
Spencer <scarvill91@gmail.com>
2015-02-19 20:46:03 -0600

    enforced uniqueness of User email attribute at the database level by adding a unique index

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150219035902) do
+ActiveRecord::Schema.define(version: 20150220023657) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -19,5 +19,7 @@ ActiveRecord::Schema.define(version: 20150219035902) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
 
 end
