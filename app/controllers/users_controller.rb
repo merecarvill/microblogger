eb0f3cef900fcb98a20fa659199da592cfac033c
Spencer <scarvill91@gmail.com>
2015-02-28 11:20:59 -0600

    added integration tests for editing users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     if @user.save
       log_in @user
       flash[:success] = "Welcome to the Sample App!"
-      redirect_to @user # redirect_to user_url(@user)
+      redirect_to @user # aka redirect_to user_url(@user)
     else
       render 'new'
     end
@@ -20,6 +20,16 @@ class UsersController < ApplicationController
 
   def edit
     @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = "Profile updated."
+      redirect_to @user
+    else
+      render 'edit'
+    end
   end
 
   private
