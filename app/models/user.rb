9ff6d6e53941f1cc7941368fd4a1a3e0606a70a3
scarvill91 <scarvill91@gmail.com>
2015-04-28 20:21:25 -0500

    created Relationship model, migration, associations, validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,10 +5,13 @@ class User < ActiveRecord::Base
   before_create :create_activation_digest
 
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name:  "Relationship",
+                                  foreign_key: "follower_id",
+                                  dependent:   :destroy
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, length: { maximum: 255 },
-                    format: { with: VALID_EMAIL_REGEX }, 
+                    format: { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
   validates :password, length: { minimum: 6 }, allow_blank: true
   has_secure_password
