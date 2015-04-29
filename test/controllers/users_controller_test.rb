d65b2d4b042e6c5dc218d834e0d2556cc56c492b
scarvill91 <scarvill91@gmail.com>
2015-04-28 20:22:17 -0500

    fixed tests to reflect change in site title format

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -15,7 +15,7 @@ class UsersControllerTest < ActionController::TestCase
   test "should get new" do
     get :new
     assert_response :success
-    assert_select "title", "Sign up | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Sign up | Microblogger"
   end
 
   test "should redirect edit when not logged in" do
